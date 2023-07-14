d6d91053172d8c7474b8d20ff505617bbb330fcf
josehernandezv <jose.andres.art@gmail.com>
2023-07-13 18:54:08 -0600

    Add custom theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,25 @@
-<script>
-	const perro = ['joey', 'may'];
-</script>
-
-<main class="flex h-screen items-center justify-center">
-	<h1 class="text-5xl font-bold text-red-500 underline">Academia de Karate Do Fenix</h1>
+<main class="flex h-screen flex-col items-center justify-center gap-6">
+	<h1 class="text-5xl font-bold text-primary underline">Academia de Karate Do Fenix</h1>
+	<div class="flex items-center gap-2">
+		<button class="btn">Button</button>
+		<button class="btn btn-neutral">Neutral</button>
+		<button class="btn btn-primary">Button</button>
+		<button class="btn btn-secondary">Button</button>
+		<button class="btn btn-accent">Button</button>
+		<button class="btn btn-ghost">Button</button>
+		<button class="btn btn-link">Button</button>
+	</div>
+	<article class="prose">
+		<h1>Garlic bread with cheese: What the science tells us</h1>
+		<p>
+			For years parents have espoused the health benefits of eating garlic bread with cheese to
+			their children, with the food earning such an iconic status in our culture that kids will
+			often dress up as warm, cheesy loaf for Halloween.
+		</p>
+		<p>
+			But a recent study shows that the celebrated appetizer may be linked to a series of rabies
+			cases springing up around the country.
+		</p>
+		<!-- ... -->
+	</article>
 </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				display: ['Reem Kufi', ...defaultTheme.fontFamily.sans],
+				// display: ['Teko', ...defaultTheme.fontFamily.sans],
+				sans: ['Montserrat', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
-	plugins: [require('daisyui')]
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				fire: {
+					primary: '#D32614',
+					secondary: '#49498A',
+					accent: '#7CBC99',
+					neutral: '#F3ECE4',
+					'base-100': '#FDFBFA',
+					'base-200': '#F8F4EF'
+				}
+			},
+			'light',
+			'dark',
+			'cupcake'
+		]
+	}
 };
