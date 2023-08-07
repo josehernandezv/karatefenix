4e29362be997eed3a16b69b7c26b049c530439ee
josehernandezv <jose.andres.art@gmail.com>
2023-08-07 11:02:11 -0600

    Hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,62 +1,25 @@
-<main class="flex flex-1 flex-col items-center justify-center gap-6">
-	<div class="hero py-12">
-		<div class="hero-content text-center">
-			<div class="max-w-md">
-				<h1 class="text-5xl font-bold">Academia de Karate Do Fenix</h1>
-				<p class="py-6">
-					Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
-					exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-				</p>
-				<button class="btn btn-primary">Get Started</button>
-			</div>
+<script lang="ts">
+	import heroImg from '$lib/images/hero-md.png';
+</script>
+
+<div class="hero pb-8">
+	<div class="hero-content flex-col lg:flex-row">
+		<img src={heroImg} alt="Karate Do" class="w-full max-w-xl lg:w-6/12 lg:max-w-5xl" />
+		<div>
+			<h1 class="text-5xl font-bold">
+				Bienvenido a <span class="text-primary">Academia de Karate Do Fénix</span>
+			</h1>
+			<p class="py-6">
+				Karate Tradicional y Deportivo impartido por instructores amigables en un ambiente seguro,
+				divertido y amigable
+			</p>
+			<button class="btn btn-primary">Comienza hoy mismo</button>
 		</div>
 	</div>
-	<div class="flex flex-wrap items-center gap-2">
-		<button class="btn">Button</button>
-		<button class="btn btn-neutral">Neutral</button>
-		<button class="btn btn-primary">Button</button>
-		<button class="btn btn-secondary">Button</button>
-		<button class="btn btn-accent">Button</button>
-		<button class="btn btn-ghost">Button</button>
-		<button class="btn btn-link">Button</button>
-	</div>
-	<article class="prose">
-		<h1>Garlic bread with cheese: What the science tells us</h1>
-		<p>
-			For years parents have espoused the health benefits of eating garlic bread with cheese to
-			their children, with the food earning such an iconic status in our culture that kids will
-			often dress up as warm, cheesy loaf for Halloween.
-		</p>
-		<p>
-			But a recent study shows that the celebrated appetizer may be linked to a series of rabies
-			cases springing up around the country.
-		</p>
-		<!-- ... -->
-	</article>
-	<article class="prose">
-		<h1>Garlic bread with cheese: What the science tells us</h1>
-		<p>
-			For years parents have espoused the health benefits of eating garlic bread with cheese to
-			their children, with the food earning such an iconic status in our culture that kids will
-			often dress up as warm, cheesy loaf for Halloween.
-		</p>
-		<p>
-			But a recent study shows that the celebrated appetizer may be linked to a series of rabies
-			cases springing up around the country.
-		</p>
-		<!-- ... -->
-	</article>
-	<article class="prose">
-		<h1>Garlic bread with cheese: What the science tells us</h1>
-		<p>
-			For years parents have espoused the health benefits of eating garlic bread with cheese to
-			their children, with the food earning such an iconic status in our culture that kids will
-			often dress up as warm, cheesy loaf for Halloween.
-		</p>
-		<p>
-			But a recent study shows that the celebrated appetizer may be linked to a series of rabies
-			cases springing up around the country.
-		</p>
-		<!-- ... -->
-	</article>
-</main>
+</div>
+
+<!-- <style lang="postcss">
+	.hero {
+		min-height: calc(100vh - 9rem);
+	}
+</style> -->
