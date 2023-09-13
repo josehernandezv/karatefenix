a074717a5ce674617cb50f083092d963f17341a5
josehernandezv <jose.andres.art@gmail.com>
2023-09-12 20:40:18 -0600

    Add scroll transitions

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -3,41 +3,70 @@
 	import Image from '$lib/components/Image.svelte';
 	import type { BlockHero, Block } from '$lib/db/queries';
 	import Circle from '$lib/components/shapes/Circle.svelte';
+	import { fade, fly } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+	import { inview } from 'svelte-inview';
 
 	export let data: Block;
 	let heroData: BlockHero = data as BlockHero;
 	type Button = { label: string; href: string };
 	let buttons = (heroData.buttons as Button[]) || [];
+
+	let isInView: boolean;
 </script>
 
-<div class="header" class:full-height={!!heroData.image}>
-	<div class="hero pb-8">
-		<div class="hero-content flex-col lg:flex-row-reverse">
-			{#if heroData.image}
-				<Image
-					file={heroData.image}
-					alt={heroData.image.title || 'Karate do'}
-					class="w-full max-w-xl lg:w-6/12 lg:max-w-5xl"
-				/>
-			{/if}
-			<div class:lg:text-center={!heroData.image} class:pt-8={!heroData.image}>
-				<h1 class="text-4xl font-bold lg:text-5xl">
-					{#if data.pre_headline}
-						{data.pre_headline}<br />
-					{/if}
-					<span class="text-5xl text-primary lg:text-7xl">{data.headline}</span>
-				</h1>
-				{#if data.content}
-					<div class="prose py-6 text-lg">
-						{@html data.content}
-					</div>
+<div
+	class="header"
+	class:full-height={!!heroData.image}
+	use:inview={{ unobserveOnEnter: true, rootMargin: '-20%' }}
+	on:inview_change={({ detail }) => {
+		isInView = detail.inView;
+	}}
+>
+	{#if isInView}
+		<div
+			class="hero pb-8"
+			in:fade={{
+				delay: 100,
+				duration: 500
+			}}
+		>
+			<div class="hero-content flex-col lg:flex-row-reverse">
+				{#if heroData.image}
+					<Image
+						file={heroData.image}
+						alt={heroData.image.title || 'Karate do'}
+						class="w-full max-w-xl lg:w-6/12 lg:max-w-5xl"
+					/>
 				{/if}
-				{#each buttons as button}
-					<a class="btn btn-primary" href={button.href || '/'}>{button.label}</a>
-				{/each}
+				<div
+					class:lg:text-center={!heroData.image}
+					class:pt-8={!heroData.image}
+					transition:fly={{
+						duration: 500,
+						x: -200,
+						opacity: 0.5,
+						easing: quintOut
+					}}
+				>
+					<h1 class="text-4xl font-bold lg:text-5xl">
+						{#if data.pre_headline}
+							{data.pre_headline}<br />
+						{/if}
+						<span class="text-5xl text-primary lg:text-7xl">{data.headline}</span>
+					</h1>
+					{#if data.content}
+						<div class="prose py-6 text-lg">
+							{@html data.content}
+						</div>
+					{/if}
+					{#each buttons as button}
+						<a class="btn btn-primary" href={button.href || '/'}>{button.label}</a>
+					{/each}
+				</div>
 			</div>
 		</div>
-	</div>
+	{/if}
 	{#if heroData.image || heroData.content}
 		<Dots class="absolute -right-40 bottom-10 hidden !h-96 !w-72 xl:block" />
 		<Circle class="absolute -bottom-8 -right-36 hidden !h-32 !w-72 xl:block" />
--- src/lib/components/MediaSection.svelte
+++ src/lib/components/MediaSection.svelte
@@ -3,64 +3,89 @@
 	import Circle from '$lib/components/shapes/Circle.svelte';
 	import Dots from '$lib/components/shapes/Dots.svelte';
 	import type { Block, BlockMediaSection } from '$lib/db/queries';
+	import { fade, fly } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+	import { inview } from 'svelte-inview';
 
 	export let data: Block;
 	let mediaSectionData: BlockMediaSection = data as BlockMediaSection;
+
+	let isInView: boolean;
 </script>
 
-<div class="hero relative pb-8">
-	<div
-		class="gap:6 hero-content flex-col lg:grid lg:grid-flow-dense lg:grid-cols-2 lg:gap-12 xl:gap-20"
-	>
-		{#if mediaSectionData.video_url}
-			<div
-				class="relative h-52 w-full max-w-xl sm:h-72 md:h-96 lg:h-full lg:max-w-5xl"
-				class:lg:col-start-2={mediaSectionData.layout === 'textmedia'}
-			>
-				<iframe
-					src={mediaSectionData.video_url}
-					frameborder="0"
-					allow="autoplay; fullscreen; picture-in-picture"
-					style="position:absolute;top:0;left:0;width:100%;height:100%;"
-					title="Video Academia Karate Fenix"
-				/>
-				<!-- <script src="https://player.vimeo.com/api/player.js"></script> -->
-			</div>
-		{/if}
-		{#if mediaSectionData.image}
-			<div
-				class="justify-self-center"
-				class:lg:col-start-2={mediaSectionData.layout === 'textmedia'}
-			>
-				<Image
-					file={mediaSectionData.image}
-					alt={mediaSectionData.image.title || 'Media logo'}
-					class="w-full max-w-lg rounded-lg drop-shadow-xl"
-				/>
-			</div>
-		{/if}
-		<div class="py-12 text-lg">
-			{#if mediaSectionData.pre_headline}
-				<p class="mb-2 text-base-content/70">{mediaSectionData.pre_headline}</p>
-			{/if}
-			<h2 class="text-4xl font-bold lg:text-5xl">{mediaSectionData.headline}</h2>
-			{#if mediaSectionData.content}
-				<div class="prose pb-3 pt-6">
-					{@html mediaSectionData.content}
+<div
+	class="hero relative min-h-[200px] pb-8"
+	use:inview={{ unobserveOnEnter: true, rootMargin: '-20%' }}
+	on:inview_change={({ detail }) => {
+		isInView = detail.inView;
+	}}
+>
+	{#if isInView}
+		<div
+			in:fade={{
+				delay: 100,
+				duration: 500
+			}}
+			class="gap:6 hero-content flex-col lg:grid lg:grid-flow-dense lg:grid-cols-2 lg:gap-12 xl:gap-20"
+		>
+			{#if mediaSectionData.video_url}
+				<div
+					class="relative h-52 w-full max-w-xl sm:h-72 md:h-96 lg:h-full lg:max-w-5xl"
+					class:lg:col-start-2={mediaSectionData.layout === 'textmedia'}
+				>
+					<iframe
+						src={mediaSectionData.video_url}
+						frameborder="0"
+						allow="autoplay; fullscreen; picture-in-picture"
+						style="position:absolute;top:0;left:0;width:100%;height:100%;"
+						title="Video Academia Karate Fenix"
+					/>
+					<!-- <script src="https://player.vimeo.com/api/player.js"></script> -->
 				</div>
 			{/if}
+			{#if mediaSectionData.image}
+				<div
+					class="justify-self-center"
+					class:lg:col-start-2={mediaSectionData.layout === 'textmedia'}
+				>
+					<Image
+						file={mediaSectionData.image}
+						alt={mediaSectionData.image.title || 'Media logo'}
+						class="w-full max-w-lg rounded-lg drop-shadow-xl"
+					/>
+				</div>
+			{/if}
+			<div
+				class="py-12 text-lg"
+				transition:fly={{
+					duration: 500,
+					x: mediaSectionData.layout === 'textmedia' ? -200 : 200,
+					opacity: 0.5,
+					easing: quintOut
+				}}
+			>
+				{#if mediaSectionData.pre_headline}
+					<p class="mb-2 text-base-content/70">{mediaSectionData.pre_headline}</p>
+				{/if}
+				<h2 class="text-4xl font-bold lg:text-5xl">{mediaSectionData.headline}</h2>
+				{#if mediaSectionData.content}
+					<div class="prose pb-3 pt-6">
+						{@html mediaSectionData.content}
+					</div>
+				{/if}
+			</div>
 		</div>
-	</div>
-	{#if mediaSectionData.layout === 'mediatext'}
-		<Circle
-			class="absolute -bottom-20 -right-20 !h-48 !w-48 xl:-right-40 xl:!h-80 xl:!w-80 2xl:-right-32 2xl:!h-96 2xl:!w-96"
-			filled
-		/>
-	{/if}
-	{#if mediaSectionData.layout === 'textmedia'}
-		<Dots class="absolute -left-44 bottom-0 xl:!h-full 2xl:-left-20" />
-		<Circle
-			class="absolute -left-16 bottom-0 xl:-left-28 xl:!h-52 xl:!w-52 2xl:-left-48 2xl:!h-96 2xl:!w-96"
-		/>
+		{#if mediaSectionData.layout === 'mediatext'}
+			<Circle
+				class="absolute -bottom-20 -right-20 !h-48 !w-48 xl:-right-40 xl:!h-80 xl:!w-80 2xl:-right-32 2xl:!h-96 2xl:!w-96"
+				filled
+			/>
+		{/if}
+		{#if mediaSectionData.layout === 'textmedia'}
+			<Dots class="absolute -left-44 bottom-0 xl:!h-full 2xl:-left-20" />
+			<Circle
+				class="absolute -left-16 bottom-0 xl:-left-28 xl:!h-52 xl:!w-52 2xl:-left-48 2xl:!h-96 2xl:!w-96"
+			/>
+		{/if}
 	{/if}
 </div>
