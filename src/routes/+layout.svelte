5408d6697adf4c0195287a6f87b2c11ab4354821
josehernandezv <jose.andres.art@gmail.com>
2024-08-28 16:35:23 -0600

    Replace background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -53,51 +53,7 @@
 
 <style lang="postcss">
 	.background {
-		background: radial-gradient(
-				at bottom right,
-				theme(colors.base-100) 0,
-				theme(colors.base-100) 2.5px,
-				hsla(0, 0%, 98%, 0.2) 0,
-				hsla(0, 0%, 98%, 0.2) 5px,
-				hsla(0, 0%, 98%, 0.75) 0,
-				hsla(0, 0%, 98%, 0.75) 7.5px,
-				hsla(0, 0%, 98%, 0.25) 0,
-				hsla(0, 0%, 98%, 0.25) 10px,
-				hsla(0, 0%, 98%, 0.3) 0,
-				hsla(0, 0%, 98%, 0.3) 12.5px,
-				hsla(0, 0%, 98%, 0.75) 0,
-				hsla(0, 0%, 98%, 0.75) 15px,
-				hsla(0, 0%, 98%, 0.2) 0,
-				hsla(0, 0%, 98%, 0.2) 17.5px,
-				transparent 0,
-				transparent 20px
-			),
-			radial-gradient(
-				at top left,
-				transparent 0,
-				transparent 2.5px,
-				hsla(0, 0%, 98%, 0.2) 0,
-				hsla(0, 0%, 98%, 0.2) 5px,
-				hsla(0, 0%, 98%, 0.75) 0,
-				hsla(0, 0%, 98%, 0.75) 7.5px,
-				hsla(0, 0%, 98%, 0.3) 0,
-				hsla(0, 0%, 98%, 0.3) 10px,
-				hsla(0, 0%, 98%, 0.25) 0,
-				hsla(0, 0%, 98%, 0.25) 12.5px,
-				hsla(0, 0%, 98%, 0.75) 0,
-				hsla(0, 0%, 98%, 0.75) 15px,
-				hsla(0, 0%, 98%, 0.2) 0,
-				hsla(0, 0%, 98%, 0.2) 17.5px,
-				theme(colors.base-100) 0,
-				theme(colors.base-100) 20px,
-				transparent 0,
-				transparent 50px
-			);
-		background-blend-mode: multiply;
-		background-color: theme(colors.base-200);
-		background-size:
-			20px 20px,
-			20px 20px;
-		/* fix pattern cropping on iOS Safari */
+		background: theme(colors.base-100) url('../lib/images/background/natural-paper.png');
+		/* background-size: contain; */
 	}
 </style>
